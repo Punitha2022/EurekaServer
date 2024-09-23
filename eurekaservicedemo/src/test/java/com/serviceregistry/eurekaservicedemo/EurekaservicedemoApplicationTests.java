5f5915b89b42624d7648301059459bb276cc23db
Punitha2022 <contactpunitha3@gmail.com>
2024-09-23 18:02:49 +0530

    commit

---
+++ eurekaservicedemo/src/test/java/com/serviceregistry/eurekaservicedemo/EurekaservicedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceregistry.eurekaservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaservicedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
